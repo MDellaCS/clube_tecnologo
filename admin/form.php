c74d1118f2af9a088522955a05804f790f6f5838
Matheus Della <matheusdellacrocce@hotmail.com>
2023-09-04 14:38:15 -0300

    Form

--- admin/form.php
+++ admin/form.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-et
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="form.css">
@@ -9,6 +9,10 @@ et
 </head>
 
 <body>
+
+    <?php
+    include_once('formValid.php');
+    ?>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" autocomplete="on">
 
@@ -35,6 +39,9 @@ et
 
                 <div>
                     <input type="text" id="nome" name="nomeTec" size="50" maxlength="150" required />
+                    <span class="error">*
+                        <?php echo $nomeErr; ?>
+                    </span>
                 </div>
             </div>
 
@@ -154,36 +161,6 @@ et
         </div>
 
     </form>
-
-    <!-- ---------------------------- VALIDAÇÃO FORMULÁRIO ---------------------------- -->
-
-    <?php
-
-    $foto = $nome = $idade = $email = $celular = $ano = $semestre = $curso = $textoPessoal = $textoFatec = $textoLivre = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $foto = test_input($_POST["fotoTec"]);
-        $nome = test_input($_POST["nomeTec"]);
-        $idade = test_input($_POST["idadeTec"]);
-        $email = test_input($_POST["emailTec"]);
-        $celular = test_input($_POST["celularTec"]);
-        $ano = test_input($_POST["anoTec"]);
-        $semestre = test_input($_POST["semestreTec"]);
-        $curso = test_input($_POST["cursoTec"]);
-        $textoPessoal = test_input($_POST["textoPessoalTec"]);
-        $textoFatec = test_input($_POST["textoFatecTec"]);
-        $textoLivre = test_input($_POST["textoLivreTec"]);
-    }
-
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    ?>
 
 </body>
 
--- admin/formResult.php
+++ admin/formResult.php
@@ -1,8 +1,9 @@
-
+<?php
 
 include_once('connection.php');
 
-$sql = "INSERT INTO tbl_tecnologo(nome, idade, ano_formacao, semestre_formacao, email, celular, curso_realizado, foto, info_sobre, info_fatec, info_area_livre) VALUES('$nomeTec', $idadeTec, $anoFormacaoTec, '$semestreTec', '$emailTec', '$celularTec', '$cursoRealizadoTec', '$fotoTec', '$textoPessoal', '$textoFatec', '$textoLivre')";
+$sql = "INSERT INTO tbl_tecnologo 
+VALUES('$nomeTec', $idadeTec, $anoFormacaoTec, '$semestreTec', '$emailTec', '$celularTec', '$cursoRealizadoTec', '$fotoTec', '$textoPessoal', '$textoFatec', '$textoLivre')";
 
 $con->query($sql);
 
