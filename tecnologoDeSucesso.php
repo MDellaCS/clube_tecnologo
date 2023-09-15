97d11d5cc8db2fb26934c287f1e58ea8072aa431
pedro <pedro@teste.com>
2023-09-15 20:40:38 -0300

    .

--- tecnologoDeSucesso.php
+++ tecnologoDeSucesso.php
@@ -39,7 +39,7 @@ $user = $result->fetch_assoc();
 <body id="bgTecnologoSucesso">
     <div id="container relativeClass">
         <img src="imagens/fzl_logo.png" id="bg" alt="">
-        <h1 class="titlePadding">Casos de sucesso da Fatec ZL</h1>
+        <h1 class="titlePadding">Casos de sucesso de Tecnólogos</h1>
         <?php if ($user) {
             //Se existir algum registro no banco de dados com o atributo 'publicado', mostra os dados deste registro na pagina
             echo "<img src='" . $user['foto'] . "' id='fotoTecnologo' alt='' >" ?>
