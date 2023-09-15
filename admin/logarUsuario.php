5eb01d9265d1c6c675d7356c2ec12f6415083e9f
pedro <pedro@teste.com>
2023-09-15 19:44:57 -0300

    tirei a criptografia do login

--- admin/logarUsuario.php
+++ admin/logarUsuario.php
@@ -7,15 +7,15 @@ $senha = $_POST['nm_senha'];
 
 $sql = "SELECT * FROM tb_admin WHERE email = ? AND senha = ?";
 $stmt = $con->prepare($sql);
-$stmt->bind_param("ss", $email, hash('sha512', $senha));
+$stmt->bind_param("ss", $email, $senha);
 $stmt->execute();
 $result = $stmt->get_result();
 $user = $result->fetch_assoc();
 
-if($user == true && $user['aprovado'] == 1){
+if($user == true){
     session_start();
     $_SESSION['login'] = "1";
-	header('Location: listaCasosSucesso.php');
+	header('Location: lista/lista.php');
 }else{
     session_start();
     $_SESSION['login'] = "0";
