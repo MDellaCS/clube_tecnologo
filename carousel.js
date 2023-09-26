5c966089f3370af914e96c1750ef4807098039fc
Matheus Della <matheusdellacrocce@hotmail.com>
2023-09-26 16:51:53 -0300

    Lista e Botões Controle

--- carousel.js
+++ carousel.js
@@ -4,9 +4,30 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         cssEase: 'cubic-bezier(0.15, 0.75, 0.55, 1.25)',
-        speed: 350,
+        speed: 450,
         arrows: false,
+        pauseOnFocus: false,
     });
 });
+
+$(document).ready(function () {
+    $('.lista').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        speed: 1,
+        arrows: false,
+        rows: 1,
+    });
+});
+
+function prevLista(){
+    $('.lista').slick('slickPrev');
+}
+
+function nextLista(){
+    $('.lista').slick('slickNext');
+}
