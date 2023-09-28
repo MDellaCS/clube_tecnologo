f443d54704b7df47af643cc9bfe3f009e1bd152e
Matheus Della <matheusdellacrocce@hotmail.com>
2023-09-28 16:10:40 -0300

    Nova Função para o controle da Lista

--- carousel.js
+++ carousel.js
@@ -9,5 +9,37 @@ $(document).ready(function () {
         speed: 450,
         arrows: false,
         pauseOnFocus: false,
+        centerMode: true,
     });
+});
+
+let currentPage = 1;
+const itemsPerPage = 24;
+let isFirstTime = true;
+
+function attLista(pagina) {
+    if (pagina === 0 && currentPage > 1) {
+        currentPage--;
+    } else if (pagina === 1 && currentPage < Math.ceil(numItens / itemsPerPage)) {
+        currentPage++;
+    }
+
+    var listaItems = document.querySelectorAll('.lista-item');
+    var startIndex = (currentPage - 1) * itemsPerPage;
+    var endIndex = startIndex + itemsPerPage;
+
+    listaItems.forEach(function (item, index) {
+        item.style.display = (index >= startIndex && index < endIndex) ? 'block' : 'none';
+    });
+
+    if (!isFirstTime) {
+        var alvoTopo = document.getElementById('topoLista');
+        if (alvoTopo) alvoTopo.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        isFirstTime = false;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    attLista(0);
 });
