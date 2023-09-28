4292f515f5a8b3a1afb4eaa17c48eaa41ec7804f
Matheus Della <matheusdellacrocce@hotmail.com>
2023-09-28 16:12:34 -0300

    Remoção echo redundante

--- recentes.php
+++ recentes.php
@@ -65,11 +65,6 @@
 
         ?>
     </div>
-    <?php
-    if ($count == 0) {
-        echo "<h1>Nenhum registro encontrado</h1>";
-    }
-    ?>
 
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
