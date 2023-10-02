e33abeb79ecd53656725072473b2d8adfc21ffaf
Matheus Della <matheusdellacrocce@hotmail.com>
2023-10-02 16:29:29 -0300

    <-- Mudar

--- admin/lista.php
+++ admin/lista.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if (!isset($_SESSION["email"]) || !isset($_SESSION["senha"])) {
+if (!isset($_SESSION["email"]) && !isset($_SESSION["senha"])) {
     header("Location: index.php");
     exit();
 } else {
